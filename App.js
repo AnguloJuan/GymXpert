884f24896dd4a27f381e278e7d2a5410638bb806
AnguloJuan <jua4_20@alu.uabcs.mx>
2023-11-29 13:11:31 -0700

    Update GluestackUIProvider config

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './AuthNavigator';
 
 export default function App() {
   return (
-    <GluestackUIProvider>
+    <GluestackUIProvider config={config}>
       <NavigationContainer>
           <AuthNavigator />
       </NavigationContainer>
