cb08547dccf85af9b649efdf541fa0543704191f
AnguloJuan <jua4_20@alu.uabcs.mx>
2023-11-30 23:17:09 -0700

    Add lucide-react-native icons to bottom tab

--- AuthNavigator.js
+++ AuthNavigator.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './screens/LoginScreen';
 import Profile from './screens/Profile';
 import Fares from './screens/Fares';
+import { Calendar, CreditCard, User } from 'lucide-react-native';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -12,9 +13,27 @@ const AuthNavigator = () => {
   const isSignedIn = true; // temporary variable
   return (
     isSignedIn ? (
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Perfil') {
+              return <User color={color} />;
+            } else if (route.name === 'Tarifas') {
+              return <CreditCard color={color} />;
+            } else if (route.name === 'Clases') {
+              return <Calendar color={color} />;
+            }
+          },
+          tabBarActiveTintColor: '#fff',
+          tabBarInactiveTintColor: '#5d596c',
+          tabBarActiveBackgroundColor: '#0077e6',
+          tabBarInactiveBackgroundColor: '#fff',
+        })}>
         <Tab.Screen name="Perfil" component={Profile} />
         <Tab.Screen name="Tarifas" component={Fares} />
+        <Tab.Screen name="Clases" component={Fares} />
       </Tab.Navigator>
     ) : (
       <Stack.Navigator>
