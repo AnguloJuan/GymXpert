8b36e0162fb8f485eced24d3b31398bc9ca6a408
AnguloJuan <jua4_20@alu.uabcs.mx>
2023-12-04 16:16:58 -0700

    Add axios dependency and fetch fares from backend

--- screens/Fares.jsx
+++ screens/Fares.jsx
@@ -1,29 +1,43 @@
-import { Box, Button, ButtonText, Center, ScrollView, Text } from "@gluestack-ui/themed";
-import { useState } from "react";
+import { Box, Button, ButtonText, Center, ScrollView, Spinner, Text } from "@gluestack-ui/themed";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import PaymentMethodModal from "../components/fares/PaymetMethodModal";
+import { BASE_URL } from "../Constants";
 
 export default function Fares() {
-    const fares = [
+    const [showPaymentModal, setShowPaymentModal] = useState(false);
+    const [fares, setFares] = useState([]);
+    /*setFares([
         {
             id: 1,
-            period: "Mensual",
+            name: "Mensual",
             cost: "$ 1.500",
             description: "Acceso a todas las clases"
         },
         {
             id: 2,
-            period: "Anual",
+            name: "Anual",
             cost: "$ 10.000",
             description: "Acceso a todas las clases"
         },
         {
             id: 3,
-            period: "Diaria",
+            name: "Diaria",
             cost: "$ 50",
             description: "Acceso a una clase"
         },
-    ];
-    const [showPaymentModal, setShowPaymentModal] = useState(false);
+    ]);*/
+
+    // Fetch fares from backend with axios
+    useEffect(() => {
+        axios.get(`${BASE_URL}/fares`)
+            .then((response) => {
+                setFares(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <>
@@ -32,11 +46,11 @@ export default function Fares() {
                     <Text fontSize={24} fontWeight="$medium" mt={40} mb={12} color="#5d596c">Tarifas de GymXpert</Text>
 
                     <Box p={24} pt={0} w={"$full"}>
-                        {fares.map((fare) => (
+                        {fares.length !== 0 ? fares.map((fare) => (
                             <Box borderColor="#5d596c" borderWidth={1} rounded={8} key={fare.id} w={"$full"} my={24}>
                                 <Box p={16}>
-                                    <Text fontSize={20} fontWeight="$medium" alignSelf="center" color="#5d596c" my={12}>{fare.period}</Text>
-                                    <Text fontSize={28} fontWeight="medium" alignSelf="center" color="$blue600" mt={12} mb={16}>{fare.cost}</Text>
+                                    <Text fontSize={20} fontWeight="$medium" alignSelf="center" color="#5d596c" my={12}>{fare.name}</Text>
+                                    <Text fontSize={28} fontWeight="medium" alignSelf="center" color="$blue600" mt={12} mb={16}>$ {fare.price}</Text>
                                     <Text color="#5d596c">{fare.description}</Text>
                                 </Box>
                                 <Button
@@ -49,15 +63,15 @@ export default function Fares() {
                                     </ButtonText>
                                 </Button>
                             </Box>
-                        ))}
+                        )) : (<Spinner />)}
                     </Box>
                     <PaymentMethodModal
-                showPaymentModal={showPaymentModal}
-                setShowPaymentModal={setShowPaymentModal}
-            />
+                        showPaymentModal={showPaymentModal}
+                        setShowPaymentModal={setShowPaymentModal}
+                    />
                 </Center>
             </ScrollView>
-            
+
         </>
     );
 }
