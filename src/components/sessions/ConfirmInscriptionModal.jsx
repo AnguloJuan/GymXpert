726a9d3c27d32fc0ace4a73309c75b46b887be3f
AnguloJuan <jua4_20@alu.uabcs.mx>
2023-12-05 01:55:19 -0700

    Fix Modals display and Add suscription

--- src/components/sessions/ConfirmInscriptionModal.jsx
+++ src/components/sessions/ConfirmInscriptionModal.jsx
@@ -1,4 +1,4 @@
-import { Button, ButtonText, CloseIcon, Heading, Modal, ModalBackdrop, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ScrollView, Text, useToast } from "@gluestack-ui/themed";
+import { Button, ButtonText, CloseIcon, Heading, Modal, ModalBackdrop, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ScrollView, Text, Toast, ToastDescription, ToastTitle, VStack, useToast } from "@gluestack-ui/themed";
 import axios from "axios";
 import { useRef } from "react";
 import { BASE_URL } from "../../../Constants";
@@ -23,66 +23,90 @@ const ConfirmInscription = (props) => {
             size="md"
             scrollBehavior="inside"
             minWidth={"$full"}
-            minHeight={"$full"}
         >
-            <ScrollView minWidth={"$full"} minHeight={"$full"}>
-
-                <ModalBackdrop h={"$full"} />
-                <ModalContent bgColor="$white" alignSelf="center" mt={"$3"} mb={"$32"} >
-                    <ModalHeader>
-                        <Heading size="lg" color="#5d596c">Inscripción a la clase</Heading>
-                        <ModalCloseButton
-                            stroke={"#5d596c"}
-                            bgColor="$red100"
-                            $hover-bg="$red300"
-                            $pressed={{
-                                bgColor: "$red300",
-                            }}
-                        >
-                            <CloseIcon />
-                        </ModalCloseButton>
-                    </ModalHeader>
-                    <ModalBody color="#5d596c" py={12}>
-                        <Text>¿Estas seguro que deseas inscribirte a esta clase?</Text>
-                        <Text fontWeight="$medium" textAlign="center">{session !== "" ? session.session.name : ""}</Text>
-                    </ModalBody>
-                    <ModalFooter pt={"$3"} pb={"$5"} >
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            action="secondary"
-                            mr="$3"
-                            onPress={() => {
-                                setShowInscriptionModal(false);
-                            }}
-                        >
-                            <ButtonText color="#5d596c">Cancelar</ButtonText>
-                        </Button>
-                        <Button
-                            size="sm"
-                            action="primary"
-                            borderWidth="$0"
-                            onPress={() => {
-                                // API call to register customer to session
-                                axios.post(`${BASE_URL}/session-days/subscribe`, {
-                                    session_day_id: session.id,
-                                    customer_id: customerId,
-                                })
-                                    .then((response) => {
-                                        console.log(response);
-                                        setShowInscriptionModal(false);
+            <ModalBackdrop />
+            <ModalContent bgColor="$white">
+                <ModalHeader>
+                    <Heading size="lg" color="#5d596c">Inscripción a la clase</Heading>
+                    <ModalCloseButton
+                        stroke={"#5d596c"}
+                        bgColor="$red100"
+                        $hover-bg="$red300"
+                        $pressed={{
+                            bgColor: "$red300",
+                        }}
+                    >
+                        <CloseIcon />
+                    </ModalCloseButton>
+                </ModalHeader>
+                <ModalBody color="#5d596c" py={12}>
+                    <Text>¿Estas seguro que deseas inscribirte a esta clase?</Text>
+                    <Text fontWeight="$medium" textAlign="center">{session !== "" ? session.session.name : ""}</Text>
+                </ModalBody>
+                <ModalFooter pt={"$3"} pb={"$5"} >
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        action="secondary"
+                        mr="$3"
+                        onPress={() => {
+                            setShowInscriptionModal(false);
+                        }}
+                    >
+                        <ButtonText color="#5d596c">Cancelar</ButtonText>
+                    </Button>
+                    <Button
+                        size="sm"
+                        action="primary"
+                        borderWidth="$0"
+                        onPress={() => {
+                            // API call to register customer to session
+                            axios.post(`${BASE_URL}/session-days/subscribe`, {
+                                session_day_id: session.id,
+                                customer_id: customerId,
+                            })
+                                .then((response) => {
+                                    toast.show({
+                                        placement: "bottom",
+                                        render: ({ id }) => {
+                                            return (
+                                                <Toast nativeId={id} action="success" variant="accent">
+                                                    <VStack space="xs">
+                                                        <ToastTitle>Clase inscrita</ToastTitle>
+                                                        <ToastDescription>
+                                                            {response.data.message}
+                                                        </ToastDescription>
+                                                    </VStack>
+                                                </Toast>
+                                            );
+                                        },
                                     })
-                                    .catch((error) => {
-                                        console.log(error);
-                                    });
-
-                            }}
-                        >
-                            <ButtonText>Inscribirse</ButtonText>
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </ScrollView>
+                                    setShowInscriptionModal(false);
+                                })
+                                .catch((error) => {
+                                    console.log(error);
+                                    toast.show({
+                                        placement: "bottom",
+                                        render: ({ id }) => {
+                                            return (
+                                                <Toast nativeId={id} action="error" variant="accent">
+                                                    <VStack space="xs">
+                                                        <ToastTitle>Error</ToastTitle>
+                                                        <ToastDescription>
+                                                            {error.response.data.message}
+                                                        </ToastDescription>
+                                                    </VStack>
+                                                </Toast>
+                                            );
+                                        },
+                                    })
+                                });
+                        }}
+                    >
+                        <ButtonText>Inscribirse</ButtonText>
+                    </Button>
+                </ModalFooter>
+            </ModalContent>
         </Modal>
 
     );
